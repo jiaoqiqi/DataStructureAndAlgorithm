5615eea7e5bdbf55c38aa7748e1dc7d7c1b28077
jiaoqiqi <1176932165@qq.com>
2016-10-20 22:33:30 +0800

    add polyn multy

--- Polyn-Multiplicat.c
+++ Polyn-Multiplicat.c
@@ -1,164 +1,173 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct Polynomial                 
+typedef struct Polyomial
 {
-	float    coef;   
-	int      expn;   
-	struct   Polynomial *next;
-}Polynomial,*Polyn;
+    float coef;         
+    int expn;           
+    struct Polyomial *next;
+} Polyomial, *Polyn;
 
-Polyn CreatePoly()                        
+Polyn CreatePloy()
 {
-	Polynomial *head,*rear,*s;
-	int c,e,n;
-
-	head=(Polynomial *)malloc(sizeof(Polynomial));
-	rear = head;
-
-	scanf("%d\n",&n);
-
-	for(int i = 1 ; i <= n ; i ++)
-	{
-		scanf("\n(%d,%d)",&c,&e);
-
-		s=(Polynomial *)malloc(sizeof(Polynomial));
-		s -> coef = c;
-		s -> expn = e;
-		rear->next = s;
-		rear = s;
-	}
-	rear -> next = NULL;
-	return(head);
-
+    Polyomial *head, *rear, *s;
+    float c;
+    int e, t;
+    char d, f;
+    head = (Polyomial *)malloc(sizeof(Polyomial));      
+    head->next = NULL;
+    rear = head;
+    scanf("%d",&t);
+    getchar();
+    while (t--) {
+        getchar();
+        scanf("%f,%d",&c,&e);
+        getchar();
+        s = (Polyomial *)malloc(sizeof(Polyomial));
+        s->coef = c;
+        s->expn = e;
+        rear->next = s;
+        rear = s;
+    }
+    rear->next = NULL;
+    return head;
 }
 
-void PrintPolyn(Polyn P)                    
+void PrintPolyn(Polyn P)
 {
-	Polyn q = P->next;
-	int flag = 1;
-	if(!q)
-	{
-		putchar('0');
-		printf("\n");
-		return ;
-	}
-	while(q)
-	{
-		if(q -> coef > 0 && flag != 1)  putchar('+');
-		if(q -> coef != 1 && q -> coef != -1)
-		{
-			printf("%g",q->coef);
-			if(q -> expn == 1)  putchar('X');
-			else if(q -> expn)  printf("X^%d",q -> expn);
-		}
-		else
-		{
-			if(q->coef == 1)
-			{
-				if(!q -> expn) putchar('1');
-				else if(q ->expn == 1 )  putchar('X');
-				else printf("X^%d",q -> expn);
-			}
-		}
-		q = q -> next;
-		flag ++;
-	}
-	printf("\n");
-}
-
-Polyn MultiplicatPolyn(Polyn h1,Polyn h2)  
-{
-	Polyn h,p,p1,p2,q;
-    p1=h1->next;
-    h=p=(Polyn)malloc(sizeof(struct Polynomial));
-    p->next=NULL;
-    while(p1)
-    {
-        p2=h2->next;
-        while(p2)
-        {
-            q=(Polyn)malloc(sizeof(struct Polynomial));
-        	q->coef=p1->coef*p2->coef;
-            q->expn=p1->expn+p2->expn;
-            p->next=q;
-            p=q;
-            p2=p2->next;
+    Polyn q = P->next;
+    int flag = 1;
+    if (!q) {
+        putchar('0');
+        printf("\n");
+        return;
+    }
+    while (q) {
+        if (q->coef>0 && flag != 1) {
+            putchar('+');
         }
-        p1=p1->next;
-    	}
-       	p->next=NULL;
-       	return(h);
+        if (q->coef != 1 && q->coef != -1) {
+            printf("%g",q->coef);
+            if (q->expn == 1) {
+                putchar('X');
+            } else if (q->expn) {
+                printf("X^%d",q->expn);
+            }
+        } else {
+            if (q->coef == 1) {
+                if (!q->expn) {
+                    putchar('1');
+                } else if (q->expn == 1) {
+                    putchar('X');
+                } else {
+                    printf("X^%d",q->expn);
+                }
+            }
+			if (q->coef == -1) {
+                if (!q->expn) {
+                    printf("-1");
+                } else if (q->expn == 1) {
+                    printf("-X");
+                } else {
+                    printf("-X^%d",q->expn);
+                }
+            }
+        }
+        q = q->next;
+        flag++;
+    }
+    printf("\n");
 }
 
-Polyn MergePolyn(Polyn h)                  
+Polyn MultiPolyn(Polyn pa,Polyn pb)
 {
-	Polyn d=h->next;
-	Polyn del,q;
-	
-	while(d)
-	{
-		q=d;
-		while(q->next)
-		{
-			if(d->expn == q->next->expn)
-			{
-				del=q->next;
-				q->coef+=del->coef;
-				q->next=del->next;
-				del->next=NULL;
-				free(del);
-			}
-			q=q->next;
+
+    Polyn qa = pa->next;
+    Polyn headc, pc, qb, qc;
+    pc = (Polyn)malloc(sizeof(struct Polyomial));
+    pc->next = NULL;
+    headc = pc;
+    while (qa != NULL) {
+        qb = pb->next;
+		while (qb != NULL) {
+		    qc = (Polyn)malloc(sizeof(struct Polyomial));
+		    qc->coef = qa->coef * qb->coef;
+		    qc->expn = qa->expn + qb->expn;
+            pc->next = qc;
+            pc = qc;
+            qb = qb->next;
 		}
-		d=d->next;
-	}
-	return h;
+        qa = qa->next;
+    }
+    pc->next = NULL;
+    return headc;
 }
-Polyn sort(Polyn head,int count)
+
+void UnitePoly(Polyomial *h)
 {
-	Polyn p;
-	int i,j,t;
-	for(i=0;i<count-1;i++)
-	{
-		p=head->next;
-		for(j=0;j<count-1-i;j++,p=p->next)
-		{
-			if(p->expn>p->next->expn)
-			{
-				t=p->expn;
-				p->expn=p->next->expn;
-				p->next->expn=t;
-				t=p->coef;
-				p->coef=p->next->coef;
-				p->next->coef=t;
+	Polyomial *pa, *pb, *qa, *qb, *temp;
+	qa = h;
+	pa = qa->next;
+
+	while(pa != NULL) {
+		pb = pa->next;
+		qb = pa;
+		while(pb != NULL) {
+			if(pa->expn == pb->expn) {
+				pa->coef = pa->coef + pb->coef;
+				if(pa->coef == 0) {
+					temp = pb;
+					qb->next = pb->next;
+					free(temp);
+					temp=pa;
+					qa->next = pa->next;
+					pa = qa;
+					free(temp);
+					break;
+				} else {
+					temp=pb;
+					qb->next = pb->next;
+					pb = pb->next;
+					free(temp);
+				}
+			} else {
+				qb = pb;
+				pb = pb->next;
+			}
+		}
+		qa = pa;
+		pa = pa->next;
+	}
+}
+
+void Insort(Polyomial *h)
+{
+	Polyomial *s, *p;
+	int t, m;
+	for (p=h->next; p!=NULL; p=p->next) {    
+		for(s=h->next; s->next!=NULL; s=s->next) {
+			if(s->expn > s->next->expn) {
+				t = s->expn;
+				m = s->coef;
+				s->coef = s->next->coef;
+				s->expn = s->next->expn;
+				s->next->coef = m;
+				s->next->expn = t;
 			}
 		}
 	}
-	return head;
 }
-int Getlen(Polyn head)
-{
-	int j=0;
-	Polyn p=head;
-	while(p->next != NULL)
-	{
-		j++;
-		p = p->next;
-	}
-	return j;
-}
-int main()
-{
-	Polynomial *head1,*head2,*head3;                  
-	head1=CreatePoly();                               
-	head2=CreatePoly();                               
-	head3=MultiplicatPolyn(head1,head2); 
 
-	head3 = sort(head3,Getlen(head1)*Getlen(head2));
-	head3=MergePolyn(head3);
-                        	
+int main(int argc, char *argv[])
+{
+    Polyomial *a, *b, *h;
 
-	PrintPolyn(head3);                                
+    a = CreatePloy();
+    b = CreatePloy();
+    h = MultiPolyn(a,b);
+    UnitePoly(h);
+	Insort(h);
+    PrintPolyn(h);
+
+    return 0;
 }
